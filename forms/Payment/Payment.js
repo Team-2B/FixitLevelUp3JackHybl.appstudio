03df969d2afea840e9f260899044709bc7a8c759
Jack Hybl <69874924+Jack-Hybl@users.noreply.github.com>
2020-10-20 11:42:22 -0500

    home loan changes

--- forms/Payment/Payment.js
+++ forms/Payment/Payment.js
@@ -3,14 +3,12 @@ function carLoanPayment(carPrinciple, carMonths, carInterest) {
   let carPayment = (carPrinciple/carMonths) * ((carInterest/100)+1)
   return carPayment
   }
-  
-/*
+
 //create home loan function
 function homeLoanPayment(homePrinciple, homeYears, homeInterest) {
-  let homePayment = (homePrinciple/homeYears * 12) * ((homeInterest/100 +1)
-  return homePayment
-}  
-*/
+  let homePayment = (homePrinciple/homeYears * 12) * ((homeInterest/100 +1))
+   return homePayment;
+  }
 
 //when user want to compute car loan
 btnAuto.onclick=function(){
@@ -22,5 +20,19 @@ btnAuto.onclick=function(){
   carMon = Number(inptMon.value)
   console.log(carInt)
   let total = carLoanPayment(carPrin, carInt, carMon)
-  lblRes.value = `Monthly payment is ${total}`
+  lblRes.value = `A car loan for $${carPrin} over ${carMon} months at ${carInt}% interest would have a monthly payment of $${total}`
 }
+
+//when user wants to computer home loan
+btnHome.onclick=function(){
+  let homePrin = ''
+  let homeInt = ''
+  let homeYear = ''
+  homePrin = Number(inptPrinc.value)
+  homeInt = (Number(inptInt.value))/100
+  homeYear = Number(inptMon.value)
+  let total1 = homeLoanPayment(homePrin, homeInt, homeYear)
+  lblRes.value = `A home loan for $${homePrin} over ${homeYear} years at ${homeInt}% interest would have a monthly payment of $${total1}`
+}
+
+
